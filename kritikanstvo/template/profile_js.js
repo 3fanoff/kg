c2c570fa89395437940e039146dcb80dfdc8de92
Michael Triff. <3fanoff@mail.ru>
2013-07-05 18:31:57 +0400

    05.07.2013 18:31

--- kritikanstvo/template/profile_js.js
+++ kritikanstvo/template/profile_js.js
@@ -1,9 +1,10 @@
 $(function(){
-	$('.lovehate_block .mode a').click(function(e){
-		e.preventDefault();
+	$loha = $('.lovehate_block');
+	$('.mode a', $loha).click(function(){
 		cls = $(this).parent().attr('class').substr(6);
-		$('.lovehate_block').removeClass('_hate').removeClass('_love');
-		$('.lovehate_block').addClass(cls);
+		$loha.removeClass('_hate').removeClass('_love');
+		$loha.addClass(cls);
+		return false;
 	});
 	
 	maxBody('.lovehate_block');
@@ -13,17 +14,15 @@ $(function(){
 			callback: function(value, link){
 				alert(value);
 			}
+			/*callback: function(value, link){
+				$.getJSON('/useractions.php?action=grade&id=574&type=-1&grade='+value+'&loginza_id=2068', function(data) {
+					$('.page_critic_user_rating').html('<h4>' + data['rating'] + '</h4>Рейтинг критика среди<br><u>посетителей &laquo;Критиканства&raquo;</u><i>' + data['votes'] + ' оцен' + ending (data['votes'], 'ка', 'ки', 'ок') + '</i>' + (data['votes']>=7?'<i>' + data['place'] + ' место в общем рейтинге</i>':''));
+					$('.page_critic_user_rating').attr('class', 'page_critic_user_rating users_rating_' + grade_color (data['rating']));
+				});
+			}*/
 		});
 	});
-	/*$('.auto-submit-star').rating({
-		callback: function(value, link){ alert(value); }
-		callback: function(value, link){
-	  		$.getJSON('/useractions.php?action=grade&id=574&type=-1&grade='+value+'&loginza_id=2068', function(data) {
-				$('.page_critic_user_rating').html('<h4>' + data['rating'] + '</h4>Рейтинг критика среди<br><u>посетителей &laquo;Критиканства&raquo;</u><i>' + data['votes'] + ' оцен' + ending (data['votes'], 'ка', 'ки', 'ок') + '</i>' + (data['votes']>=7?'<i>' + data['place'] + ' место в общем рейтинге</i>':''));
-				$('.page_critic_user_rating').attr('class', 'page_critic_user_rating users_rating_' + grade_color (data['rating']));
-			});
-		}
-	});*/
+	
 	$('.favs .mg-add').click(function(){
 		$is = $(this);
 		$.ajax({
@@ -37,6 +36,7 @@ $(function(){
 		})
 		
 	});
+	
 	$('.subs ._more_candidate').click(function(){
 		$is = $(this);
 		$.ajax({
@@ -52,6 +52,27 @@ $(function(){
 		});
 		return false;
 	});
+	
+	$field = $('form[name="status_field"] textarea');
+	if($field.length){
+		$field.charCount({allowed: 350});
+		$field.focusin(function(){
+			detect = setInterval(function(){
+				$field.trigger('keyup');
+			}, 1200);
+		});
+		$field.focusout(function(){
+			clearInterval(detect);
+		});
+		$field.keyup(statusLimit);
+	}
+	
+	$('.user_status .change').click(function(){
+		$(this).next().addClass('act');
+	});
+	$('.status_field ._close').click(function(){
+		$(this).parent().removeClass('act');
+	})
 });
 
 function sectTab(){
@@ -103,5 +124,14 @@ function maxBody(lhb){
 	arr = [ $(lhb + ' .body:eq(0)').height(), $(lhb + ' .body:eq(1)').height() ];
 	num = Math.max.apply(Math, arr);
 	$(lhb + ' .body').height(num);
+}
+
+//form[name="status_field"]
+function statusLimit(){
+	if ($(this).val().length > 350) {
+		$('input[type="submit"]', $(this).parent()).prop('disabled', true);
+	} else {
+		$('input[type="submit"]', $(this).parent()).prop('disabled', false);
+	}
 }
 
